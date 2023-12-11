ac1eba361221b04d9f3aaa8a031a0d468144f008
Khuzaima Malik <asifkhuzaima276@gmail.com>
2023-12-11 17:09:52 +0500

    The changes is updated

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Delta
 This is a demo for Git &amp; Github project
+
+# Teacher
+There is no teacher
+
+# Student
+I am learning delta project
---
+++ project.js
@@ -0,0 +1,51 @@
+console.log('The git codes i can write in the following')
+
+console.log('to see the files of the folder in which i am exist')
+console.log('ls')
+
+console.log('to enter any folder or file from git')
+console.log('cd') //space name of a folder
+
+console.log('to make folders from git')
+console.log('mkdir') //space name of a folder
+
+console.log('to make files from git')
+console.log('touch') //space name of a file
+
+console.log('to remove any file or folder from git')
+console.log('rm rf') //name of a folder or file
+
+console.log("_____________________________________________")
+
+console.log('to see the status of existing file')
+console.log('git status')
+
+console.log('to add the files of folder into github')
+console.log('git add') //space name of a file or we choose all files put .
+
+console.log('to add the comment of file')
+console.log('git commit -m write any text in comas')
+
+console.log('to push these changes on github')
+console.log('git push --set-upstream origin  branchName') //this code is used one time another time we write
+console.log('git push')
+
+console.log('to put new file on github')
+console.log('git remote add origin')
+
+console.log('to see the branch')
+console.log('git branch')
+
+console.log('to change the branch name')
+console.log('git branch -M  add branch name')
+
+console.log('This is use to add a branch')
+console.log('git checkout -b  branchName')
+
+console.log('to switch the branch')
+console.log('git checkout  branchName')
+
+console.log('to delete the branch')
+console.log('git -d  branchName')
+
+console.log('Kia hal hai')
